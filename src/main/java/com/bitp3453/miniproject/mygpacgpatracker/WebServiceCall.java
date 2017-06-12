77899e2ee92cd2d55b6e740c82407b40f66c9fb5
AmrddnBzli <amiruddin6521@gmail.com>
2017-06-12 22:54:33 +0800

    Create WebServiceCall.java

--- src/main/java/com/bitp3453/miniproject/mygpacgpatracker/WebServiceCall.java
+++ src/main/java/com/bitp3453/miniproject/mygpacgpatracker/WebServiceCall.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class WebServiceCall {
 
     JSONObject jsonObj;
-    String strUrl = "";
+    String strUrl = "asd";
 
     public WebServiceCall() {
         jsonObj = null;
